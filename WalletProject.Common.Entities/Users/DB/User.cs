e57a70ac03736d60e7bc67fcc158660469e42e4d
pollosatyi <pollosatyi@mail.ru>
2024-03-31 17:34:23 +0300

    user в работе

---
+++ WalletProject.Common.Entities/Users/DB/User.cs
@@ -0,0 +1,12 @@
+﻿namespace WalletProject.Common.Entities.Users.DB
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string Phone { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ WalletProject.Common.Entities/Users/UserInputModels/UserInputModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WalletProject.Common.Entities.Users.UserInputModels
+{
+    public class UserInputModel
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string Phone { get; set; }
+        public int Age { get; set; }
+    }
+}
