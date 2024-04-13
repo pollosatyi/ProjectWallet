e603d9ec52ebb94c3ab91d55629b8eaf6f23a838
pollosatyi <129182414+DrShadowich@users.noreply.github.com>
2024-04-13 20:21:18 +0300

    сохранение

--- WalletProject.Common.Entities/Wallets/DbWallets/Wallet.cs
+++ WalletProject.Common.Entities/Wallets/DbWallets/Wallet.cs
@@ -1,13 +1,17 @@
-﻿namespace WalletProject.Common.Entities.Wallets.DbWallet
+﻿using WalletProject.Common.Entities.Users.DB;
+
+namespace WalletProject.Common.Entities.Wallets.DbWallet
 {
     public class Wallet
     {
         public Guid Id { get; set; }
-        public Guid UserId { get; set; }
-        public double Rub { get; set; }
-        public double Usd { get; set; }
-        public double Eur { get; set; }
-        public double SumWallet { get; set; }
+        public User user { get; set; }
+        public List<BankA>
+
+        //public double Rub { get; set; }
+        //public double Usd { get; set; }
+        //public double Eur { get; set; }
+        //public double SumWallet { get; set; }
 
     }
 }
